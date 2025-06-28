b9835d7261b169bcf2aa7c3b7419fe0fa6ea58cf
Daniela-Padilha <ddo-carm@LAPTOP-REDJ8VVL.localdomain>
2025-06-28 12:13:49 +0100

    ex00 done

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -5,27 +5,34 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ddo-carm <ddo-carm@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/27 15:59:25 by ddo-carm          #+#    #+#             */
-/*   Updated: 2025/06/27 21:53:44 by ddo-carm         ###   ########.fr       */
+/*   Created: 2025/06/28 12:05:20 by ddo-carm          #+#    #+#             */
+/*   Updated: 2025/06/28 12:05:23 by ddo-carm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <string>
+#include <cstring>
 
-// void capitalize(string msg)
-// {
-// 	replace();
-// }
-
-int main(void)
+int main(int ac, char **av)
 {
-	std::string msg;
-	
-	std::getline(std::cin, msg);
-	if (msg.length() == 0)
+	int i, j;
+
+	if (ac == 1)
 		std::cout << "* LOUD AND UNBERABLE FEEDBACK NOISE *" << std::endl;
 	else
-		std::cout << "Hello world!" << std::endl;
+	{
+		i = 1;
+		while (av[i])
+		{
+			j = 0;
+			while (av[i][j])
+			{
+				std::cout << (char)toupper(av[i][j]);
+				j++;
+			}
+			i++;
+		}
+		std::cout << std::endl;
+	}
 	return (0);
 }
